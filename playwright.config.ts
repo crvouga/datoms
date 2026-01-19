53b2b472bf4aaa4525912fd1df39d7692b321f7f
Chris Vouga <crvouga@gmail.com>
2026-01-19 14:58:20 -0700

    Update Playwright configuration to change base URL and server port from 3000 to 8888. This change ensures consistency in the testing environment and aligns with the updated local development setup.

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:8888",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
@@ -34,8 +34,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "PORT=3000 bun src/app/src/server.ts",
-    url: "http://localhost:3000",
+    command: "PORT=8888 bun src/app/src/server.ts",
+    url: "http://localhost:8888",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
